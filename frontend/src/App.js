744828fc476a43cf51134be558a65e67575048b0
swift1719 <ayushpawar1501@gmail.com>
2021-06-16 22:00:46 +0530

    setup bootswatch themes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <main style={{minHeight:"44vw"}}>
+
+      </main>
+      <Footer/>
     </div>
   );
 }
