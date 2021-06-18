a44150d5291d38988b201480337777269dd3978f
swift1719 <ayushpawar1501@gmail.com>
2021-06-18 21:37:30 +0530

    added note screen

---
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -0,0 +1,89 @@
+import {
+    Button,
+    Card,
+    Badge,
+    Accordion
+} from 'react-bootstrap';
+import {
+    Link
+} from 'react-router-dom';
+import MainScreen from "../../components/MainScreen/MainScreen"
+import notes from '../../data/notes';
+
+const MyNotes = () => {
+
+    const deleteHandler=(id)=>{
+        if(window.confirm("Are you sure?")){
+
+        }
+    }
+
+    return <MainScreen title="Welcome Back User...">
+        <Link to="/createnote" >
+            <Button style={{marginLeft:10,marginBottom:6}} size="lg" color="primary" >
+                Create Note
+            </Button>
+        </Link>
+        {
+            notes.map((note,ind)=>(
+                <Accordion>
+                    <Card style={{margin:10}} >
+                        <Card.Header style={{display:"flex"}} >
+                            <span 
+                            style={{
+                                color:"black",
+                                textDecoration:"none",
+                                flex:1,
+                                cursor:"pointer",
+                                alignSelf:"center",
+                                fontSize:22,
+                            }}
+                            >
+                                {/* event key is an identifier to denote what we want to trigger with this accordian.toggle */}
+                                <Accordion.Toggle as={Card.Text} variant='link' eventKey="0" >
+                                    {note.title}
+                                </Accordion.Toggle>
+                            </span>
+                            <div>
+                            <Link to={`/note/${note._id}`} >
+                            <Button>
+                                Edit
+                            </Button>
+                            </Link>
+                            <Button 
+                            variant="danger" 
+                            className="mx-2" 
+                            onClick={()=>deleteHandler(note._id)}
+                            >
+                                Delete
+                            </Button>
+                            </div>
+                        </Card.Header>
+
+                        <Accordion.Collapse eventKey="0" >
+                            <Card.Body>
+                                <h4>
+                                    <Badge variant="success" >
+                                        Category - {note.category}
+                                    </Badge>
+                                </h4>
+                                <blockquote className="blockquote mb-0">
+                                    <p>
+                                        {note.content}
+                                    </p>
+                                    <footer className="blockquote-footer">
+                                        Create On : Date
+                                    </footer>
+                                </blockquote>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    
+                    </Card>
+                </Accordion>
+            ))
+        }
+        
+    </MainScreen>
+}
+
+export default MyNotes
