31392b8048b26195b6fd1d492a1316ce97c4417b
swift1719 <ayushpawar1501@gmail.com>
2021-06-22 20:49:47 +0530

    created register handler

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,8 @@
+const registerUser = async (req,res) =>{
+    const {name,email,password,pic} = req.body;
+    res.json({
+        name,
+        email,
+    });
+};
+module.exports = {registerUser};
