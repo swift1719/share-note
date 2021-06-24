5bac263ea59319acbc2d7f111e38e493c322571e
swift1719 <ayushpawar1501@gmail.com>
2021-06-24 20:06:36 +0530

    created login api

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser} = require('../controllers/userController');
+const {registerUser,authUser} = require('../controllers/userController');
 // route is api endpoint
 router.route('/').post(registerUser); 
+router.route('/login').post(authUser);
 
 module.exports = router;
