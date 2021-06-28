a3b15d0655c3907f5d2e83162edc136f54545a4f
swift1719 <ayushpawar1501@gmail.com>
2021-06-28 19:39:53 +0530

    implemented redux store

---
+++ frontend/src/store.js
@@ -0,0 +1,20 @@
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools } from 'redux-devtools-extension';
+import {userLoginReducer} from './reducers/userReducers';
+
+const reducer= combineReducers({
+    userLogin:userLoginReducer,
+})
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
