3d3ffc872355e8643e6936f759bc632b6bc19ce6
swift1719 <ayushpawar1501@gmail.com>
2021-06-29 17:07:20 +0530

    added registerUser reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,7 +2,10 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGOUT,
-    USER_LOGIN_FAIL
+    USER_LOGIN_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL
 } from "../constants/userConstants";
 
 
@@ -24,6 +27,28 @@ export const userLoginReducer = (state={},action) =>{
             };
         case USER_LOGOUT:
             return {};
+        default:
+            return state;
+    }
+}
+
+export const userRegisterReducer = (state={},action)=>{
+    switch (action.type) {
+        case USER_REGISTER_REQUEST:
+            return {
+                loading:true,
+                userInfo:action.payload
+            };
+        case USER_REGISTER_SUCCESS:
+            return {
+                loading:false,
+                userInfo:action.payload
+            };
+        case USER_REGISTER_FAIL:
+            return {
+                loading:false,
+                error:action.payload
+            };
         default:
             return state;
     }
