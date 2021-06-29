23f4fe2747d8eddf2f98dcb6fc53cf4cf8074999
swift1719 <ayushpawar1501@gmail.com>
2021-06-29 17:05:55 +0530

    register state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,20 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools } from 'redux-devtools-extension';
-import {userLoginReducer} from './reducers/userReducers';
+import {userLoginReducer,userRegisterReducer} from './reducers/userReducers';
 
 const reducer= combineReducers({
     userLogin:userLoginReducer,
+    userRegister:userRegisterReducer
 })
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    :null;
+
+const initialState = {
+    userLogin:{userInfo:userInfoFromStorage},
+};
 
 const middleware = [thunk];
 
