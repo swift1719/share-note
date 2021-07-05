63e1d4fc2ab00821c762979b57876aa5f1cb5360
swift1719 <ayushpawar1501@gmail.com>
2021-07-05 13:20:42 +0530

    created protected routes for create update and delete nte api

--- Backend/controllers/noteController.js
+++ Backend/controllers/noteController.js
@@ -37,4 +37,69 @@ const createNote = asyncHandler(
     }
 );
 
-module.exports = {getNotes,createNote};
+const getNoteById = asyncHandler(
+    async (req,res) =>{
+        const note = await Note.findById(req.params.id);
+
+        if(note){
+            res.json(note);
+        }else{
+            res.status(404).json({
+                message:"Note not found"
+            });
+        }
+        res.json(note);
+    }
+);
+
+const updateNote = asyncHandler(
+    async(req,res) => {
+        const {title,content,category} = req.body;
+        const note =await Note.findById(req.params.id);
+
+        if(note.user.toString() !== req.user._id.toString()){
+            res.status(401);
+            throw new Error("You can't perform this action")
+        }
+
+        if(note){
+            note.title=title;
+            note.content=content;
+            note.category=category;
+
+            const updatedNote = await note.save();
+            res.json(updatedNote);
+        }else{
+            res.status(404);
+            throw new Error("Note not found");
+        }
+    }
+);
+
+const deleteNote = asyncHandler(
+    async(req,res) => {
+
+        const note = await Note.findById(req.params.id);
+        
+        if(note.user.toString() !== req.user._id.toString()){
+            res.status(401);
+            throw new Error("You can't perform this action")
+        }
+        
+        if(note){
+            await note.remove();
+            res.json({message:"Note Deleted"});
+        }else{
+            res.status(404);
+            throw new Error("Note not found");
+        }
+    }
+);
+
+module.exports = {
+    getNotes,
+    createNote,
+    getNoteById,
+    updateNote,
+    deleteNote
+};
