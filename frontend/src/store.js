54f6f5715dde3c0aaa996541b6910eb3fd11b07f
swift1719 <ayushpawar1501@gmail.com>
2021-07-06 12:03:13 +0530

    intialize notes state with redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,12 @@ import {createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools } from 'redux-devtools-extension';
 import {userLoginReducer,userRegisterReducer} from './reducers/userReducers';
+import { noteListReducer } from './reducers/notesReducer';
 
 const reducer= combineReducers({
     userLogin:userLoginReducer,
-    userRegister:userRegisterReducer
+    userRegister:userRegisterReducer,
+    noteList:noteListReducer,
 })
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
