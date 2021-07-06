ba7c2d8b206fff05863cc96f7e095ce000794c14
swift1719 <ayushpawar1501@gmail.com>
2021-07-06 12:02:27 +0530

    added notes reducer

---
+++ frontend/src/reducers/notesReducer.js
@@ -0,0 +1,23 @@
+import { NOTE_LIST_FAIL, NOTE_LIST_REQUEST, NOTE_LIST_SUCCESS } from "../constants/notesConstants";
+
+export const  noteListReducer = (state = {notes:[]},action)=>{
+    switch(action.type){
+        case NOTE_LIST_REQUEST:
+            return {
+                loading:true
+            };
+        case NOTE_LIST_SUCCESS:
+            return {
+                loading:false,
+                notes:action.payload
+            };
+        case NOTE_LIST_FAIL:
+            return{
+                loading:false,
+                error:action.payload
+            };
+        default:
+            return state;
+    }
+};
+
