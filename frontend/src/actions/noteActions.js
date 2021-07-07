33ca81e1944e60d9c888adbd6ce6490709454118
swift1719 <ayushpawar1501@gmail.com>
2021-07-07 14:27:32 +0530

    create note action

--- frontend/src/actions/noteActions.js
+++ frontend/src/actions/noteActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { NOTE_LIST_FAIL, NOTE_LIST_REQUEST, NOTE_LIST_SUCCESS } from '../constants/notesConstants';
+import { NOTE_CREATE_FAILED, NOTE_CREATE_REQUEST, NOTE_CREATE_SUCCESS, NOTE_LIST_FAIL, NOTE_LIST_REQUEST, NOTE_LIST_SUCCESS } from '../constants/notesConstants';
 
 export const listNotes = () => async (dispatch, getState) =>{
     try{
@@ -29,6 +29,45 @@ export const listNotes = () => async (dispatch, getState) =>{
         
         dispatch({
             type:NOTE_LIST_FAIL,
+            payload:message,
+        });
+    }
+};
+
+export const createNoteAction = (title,content,category) => async (dispatch,getState) => {
+    try {
+        dispatch({
+            type:NOTE_CREATE_REQUEST
+        });
+        
+        const {
+            userLogin:{userInfo},
+        } = getState();
+
+        const config = {
+            headers:{
+                "Content-Type":"application/json",
+                Authorization:`Bearer ${userInfo.token}`,
+            }
+        };
+
+        const {data} = await axios.post(
+            `/api/notes/create`,
+            {title,content,category},
+            config
+        );
+
+        dispatch({
+            type:NOTE_CREATE_SUCCESS,
+            payload:data,
+        });
+    } catch (error) {
+        const message = error.message && error.response.data.message ?
+            error.response.data.message
+            : error.message;
+            
+        dispatch({
+            type:NOTE_CREATE_FAILED,
             payload:message,
         });
     }
