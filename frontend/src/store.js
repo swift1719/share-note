90d08d06518f15eaf5c8130b6b2e8ac3d300fc39
swift1719 <ayushpawar1501@gmail.com>
2021-07-09 12:57:02 +0530

    inititalized noteUpdate state in redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,14 @@ import {createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools } from 'redux-devtools-extension';
 import {userLoginReducer,userRegisterReducer} from './reducers/userReducers';
-import { noteCreateReducer, noteListReducer } from './reducers/notesReducer';
+import { noteCreateReducer, noteListReducer, noteUpdateReducer } from './reducers/notesReducer';
 
 const reducer= combineReducers({
     userLogin:userLoginReducer,
     userRegister:userRegisterReducer,
     noteList:noteListReducer,
     noteCreate:noteCreateReducer,
+    noteUpdate:noteUpdateReducer,
 })
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
