0bd9aaaaf17b19e53b5c755149550ffb9bf966b1
swift1719 <ayushpawar1501@gmail.com>
2021-07-12 20:38:33 +0530

    implemented search state

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,60 +1,63 @@
 import {
-    Container,
-    Navbar,
-    Nav,
-    NavDropdown, 
-    Form, 
-    FormControl, 
-} from 'react-bootstrap';
-import {
-    Link,useHistory
-} from 'react-router-dom';
-import { logout } from '../../actions/userActions';
-import {useDispatch,useSelector} from 'react-redux';
-import titleCase from '../../utils/titleCase';
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { logout } from "../../actions/userActions";
+import { useDispatch, useSelector } from "react-redux";
+import titleCase from "../../utils/titleCase";
 
-const Header = ({name="User"}) => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const userLogin = useSelector((state)=>state.userLogin);
-    const {userInfo} =userLogin;
+const Header = ({ setSearch }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const logoutHandler = ()=>{
-        dispatch(logout());
-        history.push('/');
-    }
+  const logoutHandler = () => {
+    dispatch(logout());
+    history.push("/");
+  };
 
-    return (
-        <Navbar bg="primary" expand="lg" variant="dark" >
-            <Container>
-                <div className="navbar-brand">
-                    <Link to="/" >Share Note</Link>
-                </div>
-                
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Form inline className="m-auto">
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                </Form>
-                <Nav >
-                <div className="nav-link">
-                    <Link to="/mynotes" >
-                        My Notes
-                    </Link>
-                </div>
-                <NavDropdown title={userInfo?titleCase(userInfo.name):"User"} id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#">
-                        My Profile
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item onClick={logoutHandler} >Logout</NavDropdown.Item>
-                </NavDropdown>
-                </Nav>
-                
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <div className="navbar-brand">
+          <Link to="/">Share Note</Link>
+        </div>
 
-export default Header
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Form inline className="m-auto">
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </Form>
+          <Nav>
+            <div className="nav-link">
+              <Link to="/mynotes">My Notes</Link>
+            </div>
+            <NavDropdown
+              title={userInfo ? titleCase(userInfo.name) : "User"}
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={logoutHandler}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
