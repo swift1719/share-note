4c762a7ff101efb78d43cab4fa13f512d6cb9480
swift1719 <ayushpawar1501@gmail.com>
2021-07-12 20:39:00 +0530

    search functionality included

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,135 +1,135 @@
-import { useEffect} from 'react';
-import {
-    Button,
-    Card,
-    Badge,
-    Accordion
-} from 'react-bootstrap';
-import {Link, useHistory} from 'react-router-dom';
-import MainScreen from "../../components/MainLayout/MainLayout"
-import {useDispatch,useSelector} from 'react-redux';
-import { deleteNoteAction, listNotes } from '../../actions/noteActions';
-import Loading from '../../components/Loading';
-import ErrorMessage from '../../components/ErrorMessage';
-import ReactMarkdown from 'react-markdown';
-import titleCase from '../../utils/titleCase';
+import { useEffect } from "react";
+import { Button, Card, Badge, Accordion } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import MainScreen from "../../components/MainLayout/MainLayout";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteNoteAction, listNotes } from "../../actions/noteActions";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
+import ReactMarkdown from "react-markdown";
+import titleCase from "../../utils/titleCase";
 
-const MyNotes = () => {
+const MyNotes = ({ search }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const noteList = useSelector((state) => state.noteList);
+  const userLogin = useSelector((state) => state.userLogin);
 
-    const noteList = useSelector(state => state.noteList);
-    const userLogin = useSelector(state => state.userLogin)
-    
-    const {userInfo} = userLogin;
-    const {loading,notes,error}=noteList;
+  const { userInfo } = userLogin;
+  const { loading, notes, error } = noteList;
 
-    const noteCreate = useSelector(state => state.noteCreate);
-    const {success:successCreate} = noteCreate;
+  const noteCreate = useSelector((state) => state.noteCreate);
+  const { success: successCreate } = noteCreate;
 
+  const noteUpdate = useSelector((state) => state.noteUpdate);
+  const { success: successUpdate } = noteUpdate;
 
-    const noteUpdate = useSelector(state=>state.noteUpdate);
-    const {success:successUpdate} = noteUpdate;
+  const noteDelete = useSelector((state) => state.noteDelete);
+  const { error: errorDelete, success: successDelete } = noteDelete;
 
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteNoteAction(id));
+    }
+  };
 
-    const noteDelete = useSelector(state => state.noteDelete);
-    const {error:errorDelete,success:successDelete} = noteDelete;
+  const history = useHistory();
 
-    const deleteHandler=(id)=>{
-        if(window.confirm("Are you sure?")){
-            dispatch(deleteNoteAction(id));
-        }
-    };
+  useEffect(() => {
+    dispatch(listNotes());
+    if (!userInfo) {
+      history.push("/");
+    }
+  }, [
+    history,
+    userInfo,
+    dispatch,
+    successCreate,
+    successUpdate,
+    successDelete,
+  ]);
 
-    const history = useHistory();
+  return (
+    <MainScreen
+      title={`Welcome back ${userInfo ? titleCase(userInfo.name) : "User"}...`}
+    >
+      <Link to="/createnote">
+        <Button
+          style={{ marginLeft: 10, marginBottom: 6 }}
+          size="lg"
+          color="primary"
+        >
+          Create Note
+        </Button>
+      </Link>
+      {errorDelete && (
+        <ErrorMessage variant="danger">{errorDelete}</ErrorMessage>
+      )}
+      {
+        // loadingDelete && <Loading/>
+      }
+      {error && <ErrorMessage variant="alert">{error}</ErrorMessage>}
+      {loading && <Loading />}
+      {notes
+        ?.reverse()
+        .filter((filteredNote) =>
+          filteredNote.title.toLowerCase().includes(search.toLowerCase())
+        )
+        .map((note) => (
+          <Accordion key={note._id}>
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: "flex" }}>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    cursor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 22,
+                  }}
+                >
+                  {/* event key is an identifier to denote what we want to trigger with this accordian.toggle */}
+                  <Accordion.Toggle as={Card.Text} variant="link" eventKey="0">
+                    {note.title}
+                  </Accordion.Toggle>
+                </span>
+                <div>
+                  <Link to={`/note/${note._id}`}>
+                    <Button>Edit</Button>
+                  </Link>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
 
-    useEffect(()=>{
-        dispatch(listNotes());
-        if(!userInfo){
-            history.push("/");
-        }
-    },[history,userInfo,dispatch,successCreate,successUpdate,successDelete]);
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <h4>
+                    <Badge variant="success">Category - {note.category}</Badge>
+                  </h4>
+                  <blockquote className="blockquote mb-0">
+                    <ReactMarkdown>{note.content}</ReactMarkdown>
 
-    return <MainScreen title={`Welcome back ${userInfo?titleCase(userInfo.name):"User"}...`}>
-        <Link to="/createnote" >
-            <Button style={{marginLeft:10,marginBottom:6}} size="lg" color="primary" >
-                Create Note
-            </Button>
-        </Link>
-        {
-            errorDelete && (
-                <ErrorMessage variant="danger">{errorDelete}</ErrorMessage>
-            )
-        }
-        {
-            // loadingDelete && <Loading/>
-        }
-        {error && <ErrorMessage variant="alert" >{error}</ErrorMessage>}
-        {loading && <Loading/> }
-        {   notes?.reverse().map((note)=>(
-                <Accordion key={note._id}>
-                    <Card style={{margin:10}} >
-                        <Card.Header style={{display:"flex"}} >
-                            <span 
-                            style={{
-                                color:"black",
-                                textDecoration:"none",
-                                flex:1,
-                                cursor:"pointer",
-                                alignSelf:"center",
-                                fontSize:22,
-                            }}
-                            >
-                                {/* event key is an identifier to denote what we want to trigger with this accordian.toggle */}
-                                <Accordion.Toggle as={Card.Text} variant='link' eventKey="0" >
-                                    {note.title}
-                                </Accordion.Toggle>
-                            </span>
-                            <div>
-                            <Link to={`/note/${note._id}`} >
-                            <Button>
-                                Edit
-                            </Button>
-                            </Link>
-                            <Button 
-                            variant="danger" 
-                            className="mx-2" 
-                            onClick={()=>deleteHandler(note._id)}
-                            >
-                                Delete
-                            </Button>
-                            </div>
-                        </Card.Header>
-
-                        <Accordion.Collapse eventKey="0" >
-                            <Card.Body>
-                                <h4>
-                                    <Badge variant="success" >
-                                        Category - {note.category}
-                                    </Badge>
-                                </h4>
-                                <blockquote className="blockquote mb-0">
-                                    
-                                    <ReactMarkdown>
-                                        {note.content}
-                                    </ReactMarkdown>
-                                    
-                                    <footer className="blockquote-footer">
-                                        Created On : {" "}
-                                        <cite title="Source Title">
-                                            {note.createdAt.substring(0,10)}
-                                        </cite>
-                                    </footer>
-                                </blockquote>
-                            </Card.Body>
-                        </Accordion.Collapse>
-                    
-                    </Card>
-                </Accordion>
-            ))
-        }
-        
+                    <footer className="blockquote-footer">
+                      Created On :{" "}
+                      <cite title="Source Title">
+                        {note.createdAt.substring(0, 10)}
+                      </cite>
+                    </footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        ))}
     </MainScreen>
+  );
 };
 
-export default MyNotes
+export default MyNotes;
