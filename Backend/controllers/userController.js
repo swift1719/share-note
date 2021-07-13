c2629d2a48bc01c5b4c938689d1275e664989021
swift1719 <ayushpawar1501@gmail.com>
2021-07-13 15:31:27 +0530

    added update profile controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,58 +1,84 @@
-const asyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
-const generateToken = require('../utils/generateToken');
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
 
-const registerUser = asyncHandler(async (req,res) =>{
-    // console.log(req.body);
-    const {name,email,password,pic} = req.body;
-    
-    const userExists= await User.findOne({email});
-    if(userExists){
-        res.status(400);
-        throw new Error("User already exits..")
-    }
-    const user = await User.create({
-        name,
-        email,
-        password,
-        pic,
+const registerUser = asyncHandler(async (req, res) => {
+  // console.log(req.body);
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exits..");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
     });
-
-    if(user){
-        res.status(201).json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            isAdmin:user.isAdmin,
-            pic:user.pic,
-            token:generateToken(user._id)
-        });
-    }else{
-        res.status(400);
-        throw new Error("Error while registering the user...")
-    }
-    
+  } else {
+    res.status(400);
+    throw new Error("Error while registering the user...");
+  }
 });
 
-const authUser = asyncHandler(async (req,res) =>{
-    // console.log(req.body);
-    const {email,password} = req.body;
-    
-    const user = await User.findOne({email});
+const authUser = asyncHandler(async (req, res) => {
+  // console.log(req.body);
+  const { email, password } = req.body;
 
-    if(user && (await user.matchPassword(password))){
-        res.json({
-            _id:user._id,
-            name:user.name,
-            email:user.email,
-            isAdmin:user.isAdmin,
-            pic:user.pic,
-            token:generateToken(user._id)
-        });
-    }else{
-        res.status(400);
-        throw new Error("Invalid Email or Password!");
-    }
-    
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Email or Password!");
+  }
 });
-module.exports = {registerUser,authUser};
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.pic = req.body.pic || user.pic;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      pic: updatedUser.pic,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found!!!");
+  }
+});
+
+module.exports = { registerUser, authUser, updateUserProfile };
