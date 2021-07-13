1853e8ede2ecfe26eb71f6d8f03dae5acffbc679
swift1719 <ayushpawar1501@gmail.com>
2021-07-13 15:30:59 +0530

    added update profile route

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {registerUser,authUser} = require('../controllers/userController');
+const { protect } = require("../middlewares/authMiddleware");
+const {
+  registerUser,
+  authUser,
+  updateUserProfile,
+} = require("../controllers/userController");
 // route is api endpoint
-router.route('/').post(registerUser); 
-router.route('/login').post(authUser);
+router.route("/").post(registerUser);
+router.route("/login").post(authUser);
+router.route("/profile").post(protect, updateUserProfile);
 
 module.exports = router;
