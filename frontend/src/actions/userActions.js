e4660d1520a1d0ee2244a3b83d82be1fb33e888f
swift1719 <ayushpawar1501@gmail.com>
2021-07-14 16:53:05 +0530

    update profite action creator

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,94 +1,148 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants";
-import axios from 'axios';
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_UPDATE_FAILED,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+} from "../constants/userConstants";
+import axios from "axios";
 
-export const login = (email,password) => async (dispatch) =>{
-    try {
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+    // To make api request that takes json data
+    // we need to provide some header
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-        dispatch({
-            type:USER_LOGIN_REQUEST
-        });
-            // To make api request that takes json data
-            // we need to provide some header 
-        const config = {
-            headers:{
-                "Content-type":"application/json"
-            }
-        }
+    const { data } = await axios.post(
+      "/api/users/login",
+      {
+        email,
+        password,
+      },
+      config
+    );
 
-        const {data} = await axios.post(
-            '/api/users/login',
-            {
-                email,
-                password,
-            },
-            config
-        );
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-        dispatch({
-            type:USER_LOGIN_SUCCESS,
-            payload:data
-        });
-
-        // localstorage can't store the object data
-        // so we need to convert it into string data
-        localStorage.setItem('userInfo',JSON.stringify(data))
-
-    } catch (error) {
-        dispatch({
-            type:USER_LOGIN_FAIL,
-            payload:
-            error.response && error.response.data.message
-            ?error.response.data.message:error.message
-        })
-    }
+    // localstorage can't store the object data
+    // so we need to convert it into string data
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
-export const logout = () => async (dispatch) =>{
-    localStorage.removeItem("userInfo");
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({
+    type: USER_LOGOUT,
+  });
+};
+
+export const register = (name, email, password, pic) => async (dispatch) => {
+  try {
     dispatch({
-        type:USER_LOGOUT
+      type: USER_REGISTER_REQUEST,
     });
-}
 
-export const register = (name,email,password,pic)=> async (dispatch)=>{
-    try {
-        dispatch({
-            type:USER_REGISTER_REQUEST
-        });
-        
-        const config = {
-            headers:{
-                "Content-type":"application/json",
-            },
-        };
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
 
-        const {data} = await axios.post(
-            "/api/users",
-            {
-                name,
-                email,
-                password,
-                pic
-            },
-            config
-        );
+    const { data } = await axios.post(
+      "/api/users",
+      {
+        name,
+        email,
+        password,
+        pic,
+      },
+      config
+    );
 
-        dispatch({
-            type:USER_REGISTER_SUCCESS,
-            payload:data
-        });
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
 
-        dispatch({
-            type:USER_LOGIN_SUCCESS,
-            payload:data
-        });
-        localStorage.setItem("userInfo",JSON.stringify(data));
-    } catch (error) {
-        dispatch({
-            type:USER_REGISTER_FAIL,
-            payload:
-            error.response && error.response.data.message?
-            error.response.data.message:error.message
-        });
-    }
-}
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post("/api/users/profile", user, config);
+
+    dispatch({
+      type: USER_UPDATE_SUCCESS,
+      payload: data,
+    });
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+
+    dispatch({
+      type: USER_UPDATE_FAILED,
+      payload: message,
+    });
+  }
+};
