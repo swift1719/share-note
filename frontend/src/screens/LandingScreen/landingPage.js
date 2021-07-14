836d54b0dacc04994fdf1cd174307e9b02848de6
swift1719 <ayushpawar1501@gmail.com>
2021-07-14 16:58:22 +0530

    conditional routing

--- frontend/src/screens/LandingScreen/landingPage.js
+++ frontend/src/screens/LandingScreen/landingPage.js
@@ -1,49 +1,45 @@
-import { 
-    Container,
-    Row,
-    Button
-} from 'react-bootstrap';
-import './landingPage.css'
+import { useEffect } from "react";
+import { Container, Row, Button } from "react-bootstrap";
+import "./landingPage.css";
 
-const LandingPage = () => {
+const LandingPage = ({ history }) => {
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
+    if (userInfo) {
+      history.push("/mynotes");
+    }
+  }, [history]);
 
-    // useEffect(() => {
-    //     const userInfo = localStorage.getItem("userInfo");
-    //     if(userInfo){
-    //         history.pushState("/mynotes");
-    //     }
-    // }, [history])
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to Share Note!!!</h1>
+              <p className="subtitle">Get and share notes</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button size="lg" className="landingButton">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landingButton"
+                  variant="outline-primary"
+                >
+                  Signup
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-    return (
-        <div className="main">
-            <Container>
-                <Row>
-                    <div className="intro-text">
-                        <div>
-                            <h1 className="title" >
-                                Welcome to Share Note!!!
-                            </h1>
-                            <p className="subtitle" >
-                                Get and share notes
-                            </p>
-                        </div>
-                        <div className="buttonContainer" >
-                            <a href="/login" >
-                                <Button size="lg" className="landingButton" >
-                                    Login
-                                </Button>
-                            </a>
-                            <a href="/register">
-                                <Button size="lg" className="landingButton" variant="outline-primary" >
-                                    Signup
-                                </Button>
-                            </a>
-                        </div>
-                    </div>
-                </Row>
-            </Container>
-        </div>
-    )
-}
-
-export default LandingPage
+export default LandingPage;
