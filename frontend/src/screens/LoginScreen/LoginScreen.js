311c9b8314549a8676f29a1d56d8774797676d37
swift1719 <ayushpawar1501@gmail.com>
2021-07-15 12:32:10 +0530

    restyled forms

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,86 +1,82 @@
-import MainScreen from "../../components/MainLayout/MainLayout"
-import {Form,
-    Button,
-    Row,
-    Col,
-} from 'react-bootstrap';
+import MainScreen from "../../components/MainLayout/MainLayout";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './LoginScreen.css';
+import "./LoginScreen.css";
 import { useEffect, useState } from "react";
-import {useDispatch,useSelector} from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 import { login } from "../../actions/userActions";
 
-const LoginScreen = ({history}) => {
+const LoginScreen = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
+  // to access redux app state
+  const userLogin = useSelector((state) => state.userLogin);
 
-    const dispatch = useDispatch(); 
+  const { loading, error, userInfo } = userLogin;
 
-    // to access redux app state
-    const userLogin = useSelector(state => state.userLogin)
-
-    const {loading, error, userInfo} = userLogin;
-
-    // if user already logged in then direct him to landing page
-    useEffect(()=>{
-        if(userInfo){
-            history.push('/mynotes')
-        }
-    },[history,userInfo])
-
-    const submitHandler =  async (e) =>{
-        e.preventDefault();
-        dispatch(login(email,password));
+  // if user already logged in then direct him to landing page
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/mynotes");
     }
+  }, [history, userInfo]);
 
-    return (
-        <MainScreen title="LOGIN" >
-            <div className="loginContainer">
-                {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-                {loading && <Loading/>}
-            <Form onSubmit={submitHandler} >
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control 
-                type="email" 
-                placeholder="Enter email" 
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-                autoComplete="true"
-                />
-            </Form.Group>
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
 
-            <Form.Group   controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control 
-                type="password" 
-                placeholder="Password" 
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-                autoComplete="true"
-                />
-            </Form.Group>
-            <div className="loginButton">
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </div>
-            
+  return (
+    <MainScreen title="LOGIN">
+      <div className="loginContainer">
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {loading && <Loading />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              className="input"
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="true"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              className="input"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="true"
+            />
+          </Form.Group>
+          <div className="loginButton">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </div>
         </Form>
-        <Row className="py-3 mx-auto" >
-            <Col>
-                New Customer ? <Link to="/register">Register Here</Link>
-            </Col>
-        </Row>    
-                
-        </div>        
+        <Row className="py-3 mx-auto">
+          <Col>
+            New on Share Note ?{" "}
+            <Link to="/register" style={{ color: "blue" }}>
+              Register Here
+            </Link>
+          </Col>
+        </Row>
+      </div>
     </MainScreen>
-        
-    )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
--- frontend/src/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -1,152 +1,158 @@
-import MainScreen from "../../components/MainLayout/MainLayout"
-import {Form,Row,Col,Button} from 'react-bootstrap';
-import {Link, useHistory} from 'react-router-dom';
+import MainScreen from "../../components/MainLayout/MainLayout";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
-import {useDispatch,useSelector} from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import ErrorMessage from "../../components/ErrorMessage";
 import Loading from "../../components/Loading";
 import { register } from "../../actions/userActions";
 
 const RegisterScreen = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [pic, setPic] = useState(
+    "http://www.newdesignfile.com/postpic/2013/01/generic-user-icon-windows_321380.png"
+  );
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [picMessage, setPicMessage] = useState(null);
+  const [message, setMessage] = useState(null);
 
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [pic, setPic] = useState(
-        "http://www.newdesignfile.com/postpic/2013/01/generic-user-icon-windows_321380.png"
-    );
-    const [password,setPassword] = useState("");
-    const [confirmPassword,setConfirmPassword] = useState("");
-    const [picMessage,setPicMessage] = useState(null);
-    const [message,setMessage] = useState(null);
-    
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const userRegister = useSelector(state => state.userRegister);
+  const userRegister = useSelector((state) => state.userRegister);
 
-    const {loading,error,userInfo} = userRegister;
+  const { loading, error, userInfo } = userRegister;
 
-    useEffect(()=>{
-        if(userInfo){
-            history.push("/mynotes");
-        }
-    },[history,userInfo]);
-    
-    const submitHandler = async (e)=>{
-        e.preventDefault();
-        if(password!==confirmPassword){
-            setMessage("Password does not match")
-        }else{
-            dispatch(register(name,email,password,pic));
-        }
-    };
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/mynotes");
+    }
+  }, [history, userInfo]);
 
-    const postDetails = (picture) =>{
-        
-        if(!picture){
-            return setPicMessage("Please Select a profile picture!");
-        }
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Password does not match");
+    } else {
+      dispatch(register(name, email, password, pic));
+    }
+  };
 
-        setPicMessage(null);
-        //transforming selected image using cloudinary
-        // which provides url for the stored image
-        if(picture.type==='image/jpeg' || picture.type==='image/jpg' || picture.type==='image/png'){
-            const data = new FormData();
-            data.append('file',picture);
-            data.append('upload_preset','shareNote');
-            data.append('cloud_name','imgstoreap');
-            fetch('https://api.cloudinary.com/v1_1/imgstoreap/image/upload',{
-                method:'post',
-                body:data,
-            })
-            .then(res=>res.json())
-            .then(res=>{
-                console.log(res);
-                setPic(res.url.toString());
-            }).catch((err)=>{
-                console.log(err);
-            });
-        }else{
-            return setPicMessage("Please select an image file");
-        }
+  const postDetails = (picture) => {
+    if (!picture) {
+      return setPicMessage("Please Select a profile picture!");
     }
 
-    return (
-        <MainScreen title="REGISTER" >
-            <div className="loginContainer">
-                {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-                {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
-                {loading && <Loading/>}
-                <Form onSubmit={submitHandler} >
-                    <Form.Group controlId="name">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control
-                        type="name"
-                        value={name}
-                        placeholder="Enter name"
-                        onChange={(e)=>setName(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            value={email}
-                            placeholder="Enter email"
-                            onChange={(e)=>setEmail(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword" >
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                        type="password"
-                        value={password}
-                        placeholder="Password"
-                        autoComplete="true"
-                        onChange={(e)=>setPassword(e.target.value)}
-                        />  
-                    </Form.Group>
-                    <Form.Group controlId="confirmPassword" >
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control
-                        type="password"
-                        value={confirmPassword}
-                        autoComplete="true"
-                        placeholder="Confirm password"
-                        onChange={(e)=>setConfirmPassword(e.target.value)}
-                        />  
-                    </Form.Group>
-                    {
-                        picMessage && (
-                            <ErrorMessage variant="danger">
-                                {picMessage}
-                            </ErrorMessage>
-                        )
-                    }
-                    <Form.Group controlId="pic" >
-                        <Form.Label>Profile Picture</Form.Label>
-                        <Form.File
-                        id="custom-file"
-                        type="image/png"
-                        label="Upload Profile Picture"
-                        custom
-                        onChange={(e)=>postDetails(e.target.files[0])}
-                        />  
-                    </Form.Group>
-                    <div className="loginButton">
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </div>
-                </Form>
-                <Row className="py-3 mx-auto" >
-                    <Col>
-                        Already a Customer ? <Link to="/login">Login Here</Link>
-                    </Col>
-                </Row>
-            </div>
-        </MainScreen>
-    )
-}
+    setPicMessage(null);
+    //transforming selected image using cloudinary
+    // which provides url for the stored image
+    if (
+      picture.type === "image/jpeg" ||
+      picture.type === "image/jpg" ||
+      picture.type === "image/png"
+    ) {
+      const data = new FormData();
+      data.append("file", picture);
+      data.append("upload_preset", "shareNote");
+      data.append("cloud_name", "imgstoreap");
+      fetch("https://api.cloudinary.com/v1_1/imgstoreap/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(res);
+          setPic(res.url.toString());
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      return setPicMessage("Please select an image file");
+    }
+  };
 
-export default RegisterScreen
+  return (
+    <MainScreen title="REGISTER">
+      <div className="loginContainer">
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+        {loading && <Loading />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              className="input"
+              value={name}
+              placeholder="Enter name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              className="input"
+              value={email}
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              className="input"
+              value={password}
+              placeholder="Password"
+              autoComplete="true"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              className="input"
+              type="password"
+              value={confirmPassword}
+              autoComplete="true"
+              placeholder="Confirm password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+          {picMessage && (
+            <ErrorMessage variant="danger">{picMessage}</ErrorMessage>
+          )}
+          <Form.Group controlId="pic">
+            <Form.Label>Profile Picture</Form.Label>
+            <Form.File
+              id="custom-file"
+              type="image/png"
+              label="Upload Profile Picture"
+              custom
+              onChange={(e) => postDetails(e.target.files[0])}
+            />
+          </Form.Group>
+          <div className="loginButton">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+        <Row className="py-3 mx-auto">
+          <Col>
+            Already a User ?{" "}
+            <Link to="/login" style={{ color: "blue" }}>
+              Login Here
+            </Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
+};
+
+export default RegisterScreen;
